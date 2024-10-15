a5ed970f6420a7d0305c72318613cb9f59ef1db7
nilsonxavier <nilsonxavier12@gmail.com>
2024-10-14 21:49:33 -0300

    dashboard alteracao nome

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -17,7 +17,7 @@ function Dashboard() {
             alt="Compra"
             className="w-full h-40 object-cover rounded-md"
           />
-          <h3 className="text-xl font-bold mt-4 text-center">PDV</h3>
+          <h3 className="text-xl font-bold mt-4 text-center">PDV-Sucata</h3>
         </div>
 
         {/* Outros cards podem ser adicionados aqui para Venda, Estoque, etc. */}
